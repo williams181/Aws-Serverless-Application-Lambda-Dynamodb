11e732ff2952a5837fcf90d4b87c504aadbf99b7
williams181 <william.roberto39@hotmail.com>
2023-01-18 22:29:22 -0300

    first commit

---
+++ src/main/java/br/ifpe/serverless/ApplicationServerless/ApplitacionServerless.java
@@ -0,0 +1,11 @@
+package br.ifpe.serverless.ApplicationServerless;
+
+import javax.naming.Context;
+
+public class ApplitacionServerless implements RequestHandler <Object, Object> {
+    
+
+     public Object handleRequest(Object Input, Context context) {
+    	 
+     }
+}
---
+++ src/test/java/br/ifpe/serverless/ApplicationServerless/ApplitacionServerlessTest.java
@@ -0,0 +1,20 @@
+package br.ifpe.serverless.ApplicationServerless;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApplitacionServerlessTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
